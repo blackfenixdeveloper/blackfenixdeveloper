400a0856d929dd98bb2ba7ceeceb26250b740119
blackfenixdeveloper <50924123+blackfenixdeveloper@users.noreply.github.com>
2019-05-22 21:39:26 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+export function app() {
+    console.log('app cargada')
+   /**
+    * Declarar variable */
+
+   /**
+    * Nodos del doom */
+   
+   
+   /**
+    * Manejadores de eventos */   
+   
+   }
---
+++ js/bootstrap.js
@@ -0,0 +1,11 @@
+/**
+ * PRIMERA FORMA(con funtions)
+ */
+import {app} from './app.js'
+document.addEventListener('DOMContentLoaded',app)
+//////////////////////////////////////////////
+/**
+ * SEGUNDA FORMA
+ */
+/* import {app} from './app.js'
+document.addEventListener('DOMContentLoaded', () =>new Saludador()) */
